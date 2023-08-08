346f73d8fd85a5ed461841483149e542c70ae5cf
SruthiGonti <141395823+SruthiGonti@users.noreply.github.com>
2023-08-08 17:21:48 +0530

    Update AbstractTest.java

--- src/com/qualityworkscg/tests/AbstractTest.java
+++ src/com/qualityworkscg/tests/AbstractTest.java
@@ -16,7 +16,7 @@ public abstract class  AbstractTest {
   @Parameters({"url"})
   public void setup(String url) {
     // Set the path to the geckodriver
-    System.setProperty("webdriver.gecko.driver", "./drivers/geckodriver");
+    System.setProperty("webdriver.gecko.driver", "C:/Users/000Z7A744/Downloads/geckodriver");
     
     // Instantiate a new Page and navigate 
     // to the url specified in the testng.xml
